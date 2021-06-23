0c8a50f7aeed49812ef0d8ab3650a0f73dcc59d4
Joey_M_Brown <joey.ma.brown@gmail.com>
2021-06-23 15:22:09 -0400

    Added dynamic rendering of numbers / letters to be used for chess file labeling

--- src/components/ChessBoard.vue
+++ src/components/ChessBoard.vue
@@ -1,12 +1,27 @@
 <template>
+    <div>
+        <div class="numbers"></div>
+            <ul>
+                <li :key=number v-for="number in numbers">{{ number }}</li>
+            </ul>
+        <div class="chessBoard"></div>
+        <div class="letters">
+            <ul>
+                <li :key=letter v-for="letter in letters">{{ letter }}</li>
+            </ul>
+        </div>
+    </div>
     
 </template>
 
 <script>
 export default {
   name: 'ChessBoard',
-  props: {
-    
+  data() {
+      return {
+        numbers: ['1', '2', '3', '4', '5', '6', '7', '8'],
+        letters: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+    }
   }
 }
 </script>
