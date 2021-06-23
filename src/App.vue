3545de258e241e709ec59f62c84631a0aa7d9e3e
Joey_M_Brown <joey.ma.brown@gmail.com>
2021-06-23 14:39:18 -0400

    Added CSS reset, Removed Hello World component

--- src/App.vue
+++ src/App.vue
@@ -6,12 +6,12 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ChessBoard from './components/ChessBoard.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ChessBoard
   }
 }
 </script>
@@ -21,8 +21,78 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+/*Start CSS Reset*/ 
+
+html, body, div, span, applet, object, iframe,
+h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+a, abbr, acronym, address, big, cite, code,
+del, dfn, em, img, ins, kbd, q, s, samp,
+small, strike, strong, sub, sup, tt, var,
+b, u, i, center,
+dl, dt, dd, ol, ul, li,
+fieldset, form, label, legend,
+table, caption, tbody, tfoot, thead, tr, th, td,
+article, aside, canvas, details, embed, 
+figure, figcaption, footer, header, hgroup, 
+menu, nav, output, ruby, section, summary,
+time, mark, audio, video {
+    margin: 0;
+    padding: 0;
+    font-size: 100%;
+    font: inherit;
+    vertical-align: baseline;
+}
+/* HTML5 display-role reset for older browsers */
+article, aside, details, figcaption, figure, 
+footer, header, hgroup, menu, nav, section {
+    display: block;
+}
+ol, ul {
+    list-style: none;
+}
+blockquote, q {
+    quotes: none;
+}
+blockquote:before, blockquote:after,
+q:before, q:after {
+    content: '';
+    content: none;
+}
+table {
+    border-collapse: collapse;
+    border-spacing: 0;
+}
+* {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    max-width: 100%;
+    border: 1px solid black;
+}
+/*End CSS Reset*/
+
+/*General Styles*/
+html {
+    font-size: 62.5%;
+}
+body {
+    font-size: 1.6rem;
+    color: #4F4F4F;
+    line-height: 1.5;
+}
+h1 {
+    font-size: 5rem; 
+}
+h2 {
+    font-size: 3.5rem;
+}
+section {
+    padding: 4% 0;
+}
+a {
+    text-decoration: none;
+    color: #4F4F4F; 
 }
 </style>
